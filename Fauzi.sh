2c35b68b31d4ddd0f2c1b52c1698ea1e4408b958
A14741F <42504662+A14741F@users.noreply.github.com>
2019-06-26 04:11:50 +0700

    Add files via upload

---
+++ Fauzi.sh
@@ -0,0 +1,111 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+clear
+echo $i"WELLCOME INFECTED_AOFSFOF "
+echo $i"TOOLS AKAN AKTIF"
+sleep 3
+clear
+echo $cy"++++++++++++++++++++++++++++++++++++++++++++"
+echo $cy"• TOOLS INSTALLER INFEFTED"
+echo $cy"• UPDATE 17 AGUSTUS 1945"
+echo $cy"• NAME : INFECTED_AOFSFOF"
+echo $cy"• TOOLS 310"
+echo $cy"• MR.UTOMO"
+echo $cy"• TOOLS AKTIF!!!"
+echo $cy"++++++++++++++++++++++++++++++++++++++++++++"
+echo
+sleep 3
+echo $cy"[1]"$pu" Install Bahan"
+sleep 1
+echo $cy"[2]"$me" Tidak, saya sudah install bahan"
+sleep 1
+echo $cy"[3]"$i "EXIT"
+echo
+sleep 1
+echo $cy"["$i"H"$cy"]" $i"How To Get Username & Password"
+echo $cy"["$i"I"$cy"]" $i"INFO TOOLS"
+echo
+read -p "[SELECT NUMBER] : " pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pip2 install termcolor
+pkg install ruby
+pkg install gem
+gem install lolcat
+pip2 install lolcat
+pkg install openssl
+pkg install php
+pkg install perl
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install figlet
+pkg install nano
+git clone https://github.com/A14741F/Tools
+cd Tools
+sh Tools.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/A14741F/Tools
+cd Tools
+sh Tools.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+echo "LOADING."
+sleep 1
+clear
+echo "LOADING.."
+sleep 1
+clear
+echo "LADING..."
+sleep 1
+clear
+echo "SUKSES"
+fi
+
+if [ $pil = H ]
+then
+clear
+echo $i"1. WA    : 089677556385"
+sleep 2
+echo $i"2. IG    : @Infected_AOFSFOF"
+sleep 2
+echo $i"3. web   : anonofficial.com"
+sleep 2
+echo $i"4. Gmail : cyber.crime14741@gmail.com"
+sleep 2
+echo $i"Dilarang Copyright Iya Bangsat...!!!"
+fi
+
+if [ $pil = I ]
+then
+clear
+echo $i"CREATED : Infected_AOFSFOF"
+sleep 1
+echo $i"TYPE    : TOOLS INSTALLER"
+sleep 1
+echo $i"UPDATE  : 17 agustus 1945"
+sleep 1
+echo $i"TOTAL   : 310 TOOLS"
+fi
